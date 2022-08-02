a84d63b1174d3414bd5da9aa5eb1dca1ac1c291c
Matheus F Nascimento <matheusferreira_mfn@hotmail.com>
2022-08-02 12:51:26 -0300

    useless changes in messageCreate event

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -3,10 +3,17 @@ import { BotEvent } from "src/types/BotEvent";
 
 const messageCreate = {
   name: 'messageCreate',
-  execute: (message: Message) => {
-    if(message.author.bot) return;
+  execute: async (message: Message) => {
+    if(message.author.bot || message.system) return;
 
-    message.reply('aqui quem manda é as ratazana, cuzão 🖕');
+    if(message.author.id === '315894873234472991')
+      await message.react('😎');
+
+    if(message.content.includes('caceta')){
+      const reply = await message.reply({ content: 'Message deleted due to I wanted mfs OHOHOHOH' });
+      reply.react('😎');
+      await message.delete();
+    }
   }
 
 } as BotEvent;
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,10 +1,11 @@
-import { Client } from "discord.js";
+import { ActivityType, Client } from "discord.js";
 import { BotEvent } from "src/types/BotEvent";
 
 const ready = {
   name: 'ready',
   once: true,
   execute: (client: Client) => {
+    client.user?.setActivity('twitch.tv/scalibacon', {type: ActivityType.Watching});
     console.log(`Bot ${client.user?.tag} ready for the next battle!`);
   }
 } as BotEvent;
