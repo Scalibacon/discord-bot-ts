bd1baf5a2ab7c4aef560ac26f7aa972cc8367c1e
Matheus F Nascimento <matheusferreira_mfn@hotmail.com>
2022-08-03 13:34:10 -0300

    add command to skip song

---
+++ src/commands/skip.ts
@@ -0,0 +1,32 @@
+import { getVoiceConnection } from "@discordjs/voice";
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { TypeCommand } from "src/types/Command";
+import VoiceConnectionWithPlayer from "src/types/VoiceConnectionWithPlayer";
+import { skipSong } from "./play";
+
+const skipCommand = {
+  data: new SlashCommandBuilder()
+    .setName('skip')
+    .setDescription('Skips the music'),
+
+  execute: async (interaction: ChatInputCommandInteraction) => {
+    try{
+      if(!interaction.guildId) throw new Error('No guild ID found');
+
+      const connection = getVoiceConnection(interaction.guildId) as VoiceConnectionWithPlayer | undefined;
+      
+      if(!connection) throw new Error('No voice connection found!');
+
+      await skipSong(connection);
+
+      interaction.reply('Song skipped')
+    } catch(error){
+      if(error instanceof Error){
+        console.error('Error trying to skip music', error.message);
+        interaction.reply('Error trying to skip music: ' + error.message);
+      }       
+    }
+  }
+} as TypeCommand;
+
+export default skipCommand;
